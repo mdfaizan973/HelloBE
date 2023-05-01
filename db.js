e7a1ef9812e3c93dd7c3c9420cb9a04aeb35ae2e
mdfaizan973 <faizan.md9735@gmail.com>
2023-05-01 19:56:41 +0530

    aa

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 // mongodb://127.0.0.1:27017/
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/mondayprec");
+const connection = mongoose.connect(process.env.mongoUrl);
 
 module.exports = { connection };
